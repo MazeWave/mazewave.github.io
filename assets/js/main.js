ae0a95dc50f5aac77fffb9d7872768ceb8d11153
EvanG <evan_g@orange.fr>
2020-10-16 19:06:10 +0200

    Translations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   navmenuColor();
+  localize();
 });
 
 $(function () {
@@ -9,4 +10,16 @@ $(function () {
 function navmenuColor() {
   var $nav = $("header");
   $nav.toggleClass("scrolled", $(document).scrollTop() > $("content").offset().top - $nav.height() - 30);
+}
+
+function localize() {
+  $.ajax("languages/"+(Cookies.get('language') ?? "english")+".json", { dataType: "json" }).done(function (data) {
+  $("[id*=txt-]").each(function () {
+    $(this).html(data[$(this).attr("id")]);
+  });
+  });
+}
+function changeLanguage(lang){
+    Cookies.set('language', lang);
+    localize();
 }
